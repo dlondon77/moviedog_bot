8e816ea45363538c7750df1a39db38fce3477126
dlondon77 <102523406+dlondon77@users.noreply.github.com>
2026-04-01 19:45:39 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_PATH = os.path.join(BASE_DIR, 'config', 'config.ini')
 
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(interpolation=None)
 config.read(CONFIG_PATH)
 
 logger = logging.getLogger('core.admin')
